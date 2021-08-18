389890dc5bf8ba2dd065c0eda9e78715f426c104
AmyTang <huilitang1@gmail.com>
2021-08-18 03:02:55 +0000

    get the Most Confusing Assignments

---
+++ 4_queries/MostConfusingAssignments.sql
@@ -0,0 +1,6 @@
+
+SELECT assignments.id  ,   assignments.name ,day ,assignments.chapter,count(assistance_requests.id) as total_requests 
+FROM assistance_requests
+JOIN assignments ON    assistance_requests.assignment_id = assignments.id
+GROUP BY  assignments.id 
+ORDER BY total_requests desc
