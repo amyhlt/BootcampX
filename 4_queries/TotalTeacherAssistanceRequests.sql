94b8330f1436b2fe864661bcb71ef57676e2424c
AmyTang <huilitang1@gmail.com>
2021-08-18 03:09:56 +0000

    get Total Teacher Assistance Requests

---
+++ 4_queries/TotalTeacherAssistanceRequests.sql
@@ -0,0 +1,6 @@
+ 
+SELECT count(assistance_requests.*) as total_assistances,teachers.name
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+GROUP BY teachers.name
+HAVING teachers.name='Waylon Boehm'
