b6818f8e0e7fb95633087c83b23addfa8aa2cb5c
AmyTang <huilitang1@gmail.com>
2021-08-18 21:11:28 -0700

    use parameterized queries to update students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -24,14 +24,19 @@ LIMIT 5;
   })
 });
 //commend-line
-const args=process.argv.slice(2);
-client.query(`
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts on cohorts.id=students.cohort_id
-WHERE cohorts.name like '%${args[0]}%'
-LIMIT ${args[1]};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+client.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
